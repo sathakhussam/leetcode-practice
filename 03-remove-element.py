5d7f629807930c502f029d3d145e0a3340818154
Uzam <sathakhussam@gmail.com>
2024-10-04 17:24:41 +0530

    Three down

---
+++ 03-remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = len(nums) -1
+        for i in range(k, -1, -1):
+            if nums[i] == val:
+                nums[i] = nums[k] 
+                k=k - 1
+            
+
+s = Solution()
+
+s.removeElement([3,2,3, 1,2,3], 3)
