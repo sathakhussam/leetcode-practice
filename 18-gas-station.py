8d073e3811ea59f5a6f8d751d0a01d41a3d7ca39
Uzam <sathakhussam@gmail.com>
2024-10-06 15:03:16 +0530

    17 down

---
+++ 18-gas-station.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost) -> int:
+        tg = 0
+        tc = 0
+        s = 0
+        g = 0
+
+        for i in range(len(gas)):
+            tg += gas[i]
+            tc += cost[i]
+            g += gas[i]
+            g -= cost[i]
+            if g < 0:
+                s = i
+                g = 0
+
+        if tg < tc:
+            return -1
+        return s
+
+        
+s = Solution()
+s.canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2])
+# s.canCompleteCircuit([1,2,0,3,4, 0,5], [3,4,0,5,1,0,2])
+# s.canCompleteCircuit([2],[2])
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Progress Update
 
 - I have to repeat the 10th problem, one week later 12th oct
+- Maybe have a look at 18th one as well
